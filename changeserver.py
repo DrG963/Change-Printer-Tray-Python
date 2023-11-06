82988cf234f1b792300a01b8de2371c8a6c29e47
Gavin <37956598+DrG963@users.noreply.github.com>
2023-11-06 12:59:21 -0700

    first basic version of scanner - needs updates

--- changeserver.py
+++ changeserver.py
@@ -1,6 +1,7 @@
 import win32print
 # import win32gui
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import scanner
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ device_name = "Brother MFC-J6955DW Printer"
 def change_printer_tray():
     tray_number = request.args.get('tray', type=int)
 
-    if tray_number is None:
+    if tray_number is None and tray_number != 1 and tray_number != 2:
         return "Invalid tray number", 400
 
     # Get a handle for the default printer
@@ -30,6 +31,42 @@ def change_printer_tray():
 
     # Confirm the changes were updated
     return f"Changed printer tray to {tray_number}"
+
+@app.route('/scan_and_save', methods=['POST'])
+def scan_and_save():
+    # Get data from the JSON request
+    data = request.get_json()
+    filename = data.get('filename')
+    filetype = data.get('filetype')
+    customername = data.get('customername')
+    tpcID = data.get('tpcID')
+
+    # Check if required data is missing
+    if not filename or not filetype or not customername or not tpcID:
+        return jsonify({'error': 'Missing data'}), 400
+
+    # List available scanners
+    available_scanners = scanner.Scanner().get_scanners()
+
+    if available_scanners:
+        selected_scanner = available_scanners[0]  # Choose the first available scanner
+
+        # Configure scanning parameters (e.g., resolution, color mode, etc.)
+        scan_settings = scanner.ScannerSettings()
+        scan_settings.color_mode = scanner.ColorMode.COLOR
+        scan_settings.resolution = 300  # DPI
+
+        # Queue a scan
+        scanned_image = selected_scanner.scan(scan_settings)
+
+        # Save the scanned image to a file with the provided filename and extension
+        file_path = f"{filename}.{filetype}"
+        with open(file_path, "wb") as f:
+            f.write(scanned_image)
+
+        return jsonify({'message': 'Scanned document saved successfully', 'file_path': file_path})
+    else:
+        return jsonify({'error': 'No scanners found'})
 
 if __name__ == "__main__":
     app.run(debug=True)
