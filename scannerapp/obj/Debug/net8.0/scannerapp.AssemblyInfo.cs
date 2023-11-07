c434e919961e8084abdceadfd4ae6e9ef9fb8784
Gavin <37956598+DrG963@users.noreply.github.com>
2023-11-07 12:20:39 -0700

    new build

--- scannerapp/obj/Debug/net8.0/scannerapp.AssemblyInfo.cs
+++ scannerapp/obj/Debug/net8.0/scannerapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scannerapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f20d1f4abca7d2b17d2f53d7a0bad3913cef74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2081f8cd263ce309c62d255087982f9a00d55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("scannerapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scannerapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
